e4a67fdf7a31a08548f91596248699025442c624
Naoki Kojima <133028205+nkoji21@users.noreply.github.com>
2026-09-15 16:44:42 +0900

    feat(cpp01): ex05 (Harl 2.0) を追加 (#52)

---
+++ cpp01/ex05/src/main.cpp
@@ -0,0 +1,9 @@
+#include "Harl.hpp"
+
+int main(void) {
+  Harl testcases;
+  testcases.complain("DEBUG");
+  testcases.complain("INFO");
+  testcases.complain("WARNING");
+  testcases.complain("ERROR");
+}
