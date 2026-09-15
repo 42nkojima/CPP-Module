2290b8d43e378dbd31753b824c6491a771088d4d
Naoki Kojima <133028205+nkoji21@users.noreply.github.com>
2026-09-15 18:07:24 +0900

    refactor(cpp01): apply format clang-format (#55)

--- cpp01/ex03/src/HumanB.cpp
+++ cpp01/ex03/src/HumanB.cpp
@@ -1,4 +1,5 @@
 #include "HumanB.hpp"
+
 #include <iostream>
 
 HumanB::HumanB(const std::string& name) : name_(name), weapon_(NULL) {}
@@ -9,8 +10,8 @@ void HumanB::setWeapon(const Weapon& weapon) { weapon_ = &weapon; }
 
 void HumanB::attack() const {
   if (weapon_ == NULL) {
-	std::cout << name_ << " attacks with their bare hands\n";
+    std::cout << name_ << " attacks with their bare hands\n";
   } else {
-	std::cout << name_ << " attacks with their " << weapon_->getType() << "\n";
+    std::cout << name_ << " attacks with their " << weapon_->getType() << "\n";
   }
 }
